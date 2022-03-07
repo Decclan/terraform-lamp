9eaa5f10f67a849c2af1b977203d7b5e21b4ca84
Decclan <dec.evan11@gmail.com>
2022-03-07 11:25:22 +0000

    first-commit

--- internetgateway.tf
+++
@@ -1,7 +0,0 @@
-#create internet gateway
-resource "aws_internet_gateway" "myvpc_internet_gateway" {
-  vpc_id = aws_vpc.myvpc.id
-  tags = {
-    Name = "myvpc_internet_gateway"
-  }
-}
--- privatesubnettwo.tf
+++
@@ -1,9 +0,0 @@
-#create private subnet two
-resource "aws_subnet" "myvpc_private_subnet_two" {
-  vpc_id            = aws_vpc.myvpc.id
-  cidr_block        = element(var.subnet_two_cidr, 1)
-  availability_zone = data.aws_availability_zones.availability_zones.names[1]
-  tags = {
-    Name = "myvpc_private_subnet_two"
-  }
-}
